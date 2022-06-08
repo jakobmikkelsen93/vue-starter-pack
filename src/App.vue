3b5ff992e7af18ae04b9914f479c814a464ecd70
Jakob Mikkelsen <jm@connec.dk>
2022-06-09 00:24:40 +0200

    2 commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import ProductService from './service/ProductService';
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+
+onMounted(() => {
+            productService.value.getProducts().then(data => products.value = data);
+        })
+
+        const products = ref();
+        const productService = ref(new ProductService());
+
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+    <div>
+        <DataTable :value="products" responsiveLayout="scroll">
+            <Column field="code" header="Code"></Column>
+            <Column field="name" header="Name"></Column>
+            <Column field="category" header="Category"></Column>
+            <Column field="quantity" header="Quantity"></Column>
+        </DataTable>
+    </div>
+
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/service/ProductService.js
@@ -0,0 +1,11 @@
+
+export default class ProductService {
+
+
+	getProducts() {
+		return fetch('demo/data/products.json').then(res => res.json()).then(d => d.data);
+    }
+
+}
+
+    
